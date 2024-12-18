3af4a9a2f03b477c9acc5c52fb918f0c607f6cf0
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-12-18 09:09:21 +0200

    Added mobile nav

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
+import burgerBtn from "../../assets/img/burger-btn.svg";
 
 const Header = () => {
+  const [isBurgerButtonClicked, setBurgerButtonClicked] = useState(false);
+
+  console.log(isBurgerButtonClicked);
+
+  const navbar = (
+    <ul>
+      <li>
+        <a href="#">Main</a>
+      </li>
+      <li>
+        <a href="#">Insert</a>
+      </li>
+    </ul>
+  );
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>to do app</div>
+
+      <nav className={styles.desktopNav}>{navbar} </nav>
+
+      <button onClick={() => setBurgerButtonClicked(!isBurgerButtonClicked)}>
+        <img src={burgerBtn.src} alt="burger btn" />
+      </button>
+
+      <div
+        className={`${styles.overlay}  ${
+          isBurgerButtonClicked && styles.overlayOpen
+        }`}
+      >
+        <nav className={styles.mobileNav}>{navbar} </nav>
+      </div>
     </div>
   );
 };
